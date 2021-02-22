861329a937400d836f4206f5c9d1104df990be7b
carlos-palalo <palaxcarlos@gmail.com>
2021-02-22 11:38:14 +0100

    fasdfasdf

--- src/main/java/com/carlos/PracticaVacunas/repository/VacunaRepository.java
+++ src/main/java/com/carlos/PracticaVacunas/repository/VacunaRepository.java
@@ -1,9 +1,15 @@
 package com.carlos.PracticaVacunas.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
 import com.carlos.PracticaVacunas.model.Vacuna;
 import java.util.List;
 
+import javax.transaction.Transactional;
+
+@Repository
 public interface VacunaRepository extends JpaRepository<Vacuna, Integer> {
 
 	public List<Vacuna> findByComunidad(String comunidad);
